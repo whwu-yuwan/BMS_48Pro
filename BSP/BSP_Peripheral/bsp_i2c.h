da632a0e4ff08fd317e94e70789bee1ed13222a1
Yuwan <1445811372@qq.com>
2026-04-26 21:09:10 +0800

    fix:TS1 GPIO开启

--- BSP/BSP_Peripheral/bsp_i2c.h
+++ BSP/BSP_Peripheral/bsp_i2c.h
@@ -9,5 +9,4 @@ uint8_t BSP_I2C_Read_Buffer(uint8_t dev_addr, uint8_t reg, uint8_t *buf, uint8_t
 uint8_t BSP_I2C_Write_Buffer(uint8_t dev_addr, uint8_t reg, uint8_t *buf, uint8_t len);
 uint8_t BSP_I2C_IsDeviceReady(uint8_t dev_addr);
 
-
 #endif
